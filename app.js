9170f990ab883fa6d661cfda2c0c3ff0b91c9082
Lawrence <l.marcelino007@gmail.com>
2016-06-28 16:39:08 +0800

    Implement device integration seed plugin

---
+++ app.js
@@ -0,0 +1,102 @@
+'use strict';
+
+var platform = require('./platform'),
+    client;
+
+/**
+ * Emitted when the platform issues a sync request. Means that the device integration should fetch updates from the
+ * 3rd party service.
+ */
+platform.on('sync', function (lastSyncDate) {
+    // TODO: Fetch updates from service
+
+    /* Sample Code:
+    *    client.fetchData({updated_since: lastSyncDate}, function (error, dataCollection) {
+    *        dataCollection.forEach(function (data) {
+    *            platform.upsertDevice(data);
+    *        });
+    *    });
+    */
+});
+
+platform.on('adddevice', function(device) {
+    //TODO: Add device to 3rd party service
+
+    /* Sample Code:
+    *    client.addDevice(device, function() {
+    *        console.log('New device added');
+    *    });
+    */
+
+});
+
+platform.on('updatedevice', function(device) {
+    //TODO: Update device from 3rd party service
+
+    /*  Sample Code:
+    *   client.updateDevice(device, function() {
+    *        console.log(`${device.name} has been updated`);
+    *    });
+    */
+});
+
+platform.on('removedevice', function(device) {
+    //TODO: Remove device from 3rd party service
+
+    /* Sample Code:
+    *    client.removeDevice(device._id, function() {
+    *    console.log(`${device.name} has been removed`);
+    *    });
+    */
+});
+
+/**
+ * Emitted when the platform shuts down the plugin. The Device Integration should perform cleanup of the resources on this event.
+ */
+platform.once('close', function () {
+    let d = require('domain').create();
+
+    d.once('error', function (error) {
+        console.error(error);
+        platform.handleException(error);
+        platform.notifyClose();
+        d.exit();
+    });
+
+    d.run(function () {
+        // TODO: Release all resources and close connections etc.
+        platform.notifyClose(); // Notify the platform that resources have been released.
+        d.exit();
+    });
+});
+
+/**
+ * Emitted when the platform bootstraps the plugin. The plugin should listen once and execute its init process.
+ * Afterwards, platform.notifyReady() should be called to notify the platform that the init process is done.
+ * @param {object} options The parameters or options. Specified through config.json.
+ */
+platform.once('ready', function (options) {
+    /*
+     * Initialize your device integration using the options. See config.json
+     * You can customize config.json based on the needs of your plugin.
+     * Reekoh will inject these configuration parameters as options here in the ready event.
+     *
+     * Note: Option Names are based on what you specify on the config.json.
+     */
+
+    // TODO: Initialize your client or subscribe to the 3rd party service here.
+
+    /*
+     * Sample Code
+     *
+     * var service = require('service');
+     *
+     * service.connect(options, function (error, serviceClient) {
+     * 	client = serviceClient;
+     * });
+     */
+
+    console.log(options);
+    platform.notifyReady();
+    platform.log('Device integration has been initialized.');
+});
---
+++ platform.js
@@ -0,0 +1,189 @@
+'use strict';
+
+/**
+ * Utility function to validate String Objects
+ * @param val The value to be evaluated.
+ * @returns {boolean}
+ */
+var isString = function (val) {
+    return typeof val === 'string' || ((!!val && typeof val === 'object') && Object.prototype.toString.call(val) === '[object String]');
+};
+
+/**
+ * Utility function to validate Error Objects
+ * @param val The value to be evaluated.
+ * @returns {boolean}
+ */
+var isError = function (val) {
+    return (!!val && typeof val === 'object') && typeof val.message === 'string' && Object.prototype.toString.call(val) === '[object Error]';
+};
+
+/**
+ * Main object used to communicate with the platform.
+ * @returns {Platform}
+ * @constructor
+ */
+function Platform() {
+    if (!(this instanceof Platform)) return new Platform();
+
+    require('events').EventEmitter.call(this);
+    Platform.init.call(this);
+}
+
+require('util').inherits(Platform, require('events').EventEmitter);
+
+/**
+ * Init function for Platform.
+ */
+Platform.init = function () {
+    ['SIGHUP', 'SIGINT', 'SIGTERM'].forEach((signal) => {
+        process.on(signal, () => {
+        console.log(`Executing ${signal} listener...`);
+            this.emit('close');
+
+            setTimeout(() => {
+                this.removeAllListeners();
+                process.exit();
+            }, 2000);
+        });
+    });
+
+    ['unhandledRejection', 'uncaughtException'].forEach((exceptionEvent) => {
+            process.on(exceptionEvent, (error) => {
+            console.error(exceptionEvent, error);
+            this.handleException(error);
+            this.emit('close');
+
+            setTimeout(() => {
+                this.removeAllListeners();
+                process.exit(1);
+            }, 2000);
+        });
+    });
+
+    /**
+    *   Available events triggered by the main instance ['ready', 'close', 'adddevice', 'updatedevice', 'removedevice']
+    */
+    process.on('message', (m) => {
+        let type = m.type;
+
+        switch (type) {
+            case 'ready':
+                this.emit(type, m.data.options);
+                break;
+            case 'close':
+                this.emit(type);
+                break;
+            default:
+                this.emit(type, m.data);
+        }
+    });
+};
+
+/**
+ * Add or update 3rd party service devices to Reekoh devices.
+ * @param {object} [device] Device metadata.
+ * @param {function} [callback] Optional callback to be called once the signal has been sent.
+ */
+Platform.prototype.upsertDevice = function(device, callback) {
+    callback = callback || function () {};
+
+    setImmediate(() => {
+        process.send({
+            type: 'upsertdevice',
+            data: device
+        }, callback);
+    });
+};
+
+/**
+ * Remove device from Reekoh devices collection.
+ * @param {string} deviceId Device indentifier.
+ * @param {function} [callback] Optional callback to be called once the signal has been sent.
+ */
+Platform.prototype.removeDevice = function(deviceId, callback) {
+    callback = callback || function () {};
+
+    setImmediate(() => {
+        if (!deviceId || !isString(deviceId)) return callback(new Error('A valid client/device identifier is required.'));
+
+        process.send({
+            type: 'removedevice',
+            data: {
+                _id: deviceId
+            }
+        }, callback);
+    });
+};
+
+/**
+ * Needs to be called once in order to notify the platform that the plugin has already finished the init process.
+ * @param {function} [callback] Optional callback to be called once the ready signal has been sent.
+ */
+Platform.prototype.notifyReady = function (callback) {
+    callback = callback || function () {
+        };
+
+    setImmediate(() => {
+        process.send({
+            type: 'ready'
+        }, callback);
+    });
+};
+
+/**
+ * Notifies the platform that resources have been released and this plugin can shutdown gracefully.
+ * @param {function} [callback] Optional callback to be called once the close signal has been sent.
+ */
+Platform.prototype.notifyClose = function (callback) {
+    callback = callback || function () {
+        };
+
+    setImmediate(() => {
+        process.send({
+            type: 'close'
+        }, callback);
+    });
+};
+
+/**
+ * Logs any data to the attached loggers in the topology.
+ * @param {string} data The data that needs to be logged.
+ * @param {function} callback Optional callback to be called once the data has been sent.
+ */
+Platform.prototype.log = function (data, callback) {
+    callback = callback || function () {
+        };
+
+    if (!data || !isString(data)) return callback(new Error('A valid log data is required.'));
+
+    process.send({
+        type: 'log',
+        data: data
+    }, callback);
+};
+
+/**
+ * Logs errors to all the attached exception handlers in the topology.
+ * @param {error} error The error to be handled/logged
+ * @param {function} callback Optional callback to be called once the error has been sent.
+ */
+Platform.prototype.handleException = function (error, callback) {
+    callback = callback || function () {
+        };
+
+    setImmediate(() => {
+        if (!isError(error)) return callback(new Error('A valid error object is required.'));
+
+        process.send({
+            type: 'error',
+            data: {
+                name: error.name,
+                message: error.message,
+                stack: error.stack
+            }
+        }, callback);
+    });
+};
+
+module.exports = new Platform();
